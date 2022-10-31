b695be1e5310e95df3a598da5e1bc404fa4351b0
RAND ALHASSOUN <rialhassoun22@twq.idserve.net>
2022-10-31 13:00:07 +0300

    nk

--- mine Project/TabBar.swift
+++ mine Project/TabBar.swift
@@ -13,7 +13,7 @@ struct TabBar: View {
         TabView{
             
             
-            SecondView().tabItem{Image(systemName: "square.grid.3x3.square")
+            ContentView().tabItem{Image(systemName: "square.grid.3x3.square")
                 Text("Schedule")
             }
             
