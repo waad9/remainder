c5bbbd13600cfd89f1940c647960ade07f4196b6
githup <waalruwaili22@twq.idserve.net>
2022-11-02 11:22:57 +0300

    Initial Commit

---
+++ Mini1 project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mini1 project
+//
+//  Created by وعد عياد الرويلي on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
